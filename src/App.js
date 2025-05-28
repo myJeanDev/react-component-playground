3d61a1909bb7cbb6a35badca879c84b4c64668b3
Willeujeans <112419867+Willeujeans@users.noreply.github.com>
2025-05-28 12:21:46 -0600

    Update App.js to contain new components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import ProjectCard from './components/ProjectCard';
-import Button from './components/Button';
 import MouseTracker from './components/MouseTracker';
-import { useState } from 'react';
+import Counter from './components/Counter';
 
 function App() {
-  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
-
-  const handleButtonClick = (value) => {
-    alert(`You clicked: ${value}`);
-  };
-
   return (
     <div className="App">
       <header className="App-header">
+        <Counter />
         <ProjectCard />
-        <Button value="Delete" onClick={handleButtonClick} /> {/*  */}
-        <MouseTracker onMouseMove={setCoordinates} /> {/* component that returns the mouse position, `callback function={state-updater function}` */}
+        <MouseTracker /> {/* component that returns the mouse position, `callback function={state-updater function}` */}
       </header>
     </div>
   );
